315eadb67f33781bacef937ac73e1ab394685b26
zainimran <zainimran@outlook.com>
2020-02-03 04:47:54 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
-	consumer_key:         'yVpkMTz15UHwpJcwYljnoE7zI',
-	consumer_secret:      'xLMa2YVQDgHrOP04T1pdIEA4CDdmVQe6y3ZEMfqCgS6sEL3rV5',
-	access_token:         '146356989-tzBJi352PLZywXWe0fU62oOOlqC4E3KA19ZORQk0',
-	access_token_secret:  'G4EkB5xU3wpTfDwKnBwiuFccG4xLGRuvvdn8zL3xEf2Bb'
+	consumer_key:         'D6yOSPse6mutpiKdFYQ2zkSgi',
+	consumer_secret:      'ysCIFRIQlwXbo0wvAU7MN4NoWDElAwoKEzCWekMXCKbuWnjmrY',
+	access_token:         '2946705756-7FRzrDtslnpX1eXWdu2qHI7PlJkxSF8rbku4S7Z',
+	access_token_secret:  'iY90hE2sTDDNlHanWKwwNV2lLIblSeD8Vn2F5PML2PwcY'
 }
